d246c805b9d784958696cce131f0214cbe961f1e
vinay1074 <102849221+vinay1074@users.noreply.github.com>
2024-04-21 19:38:56 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-check=$"- Error: fail"
-text=$(cat scan-report.yaml | grep -i  Error)
+check="- Error: fail"
+text=$(cat scan-report.yaml | grep -i  Error | head -n 1)
 if test "$text" = "$check"
 then
    echo "$text"
 else
    exit 0
 fi
-exit 1 
+exit 1
